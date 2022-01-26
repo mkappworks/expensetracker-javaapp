c789192f425cf4b16c89b5a1936adb5e5370a26a
Asiri Indatissa <asiri@unionassurance.com>
2022-01-26 22:40:02 +0530

    Add FileDataAdd save data format

--- src/service/FileDataAdd.java
+++ src/service/FileDataAdd.java
@@ -1,25 +1,26 @@
 package service;
-import java.io.FileWriter;  
+
+import java.io.FileWriter;
 import java.io.IOException;
 import java.util.ArrayList;
-public class FileDataAdd extends FileManager{
 
-  public void fileOperation(String filePath){
+public class FileDataAdd extends FileManager {
+
+  public void fileOperation(String filePath) {
     ArrayList<ArrayList<String>> fileRecordArray = this.getFileRecordArray();
-    try
-   {
-    FileWriter fw = new FileWriter(filePath,true); //the true will append the new data
-    for(ArrayList<String> innerList : fileRecordArray) {
-      for(String str : innerList) {
-          System.out.println(str);
-          fw.write(str);     
+    try {
+      FileWriter fw = new FileWriter(filePath, true); // the true will append the new data
+
+      for (ArrayList<String> innerList : fileRecordArray) {
+        for (String str : innerList) {
+          fw.write(str);
+          fw.write("\t");
+        }
+        fw.write("\n");
       }
-    }
-          fw.close();
-  }
-  catch(IOException ioe)
-  {
+      fw.close();
+    } catch (IOException ioe) {
       System.err.println("IOException: " + ioe.getMessage());
-  }
     }
   }
+}
