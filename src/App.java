02644de96e3072bace41283fbff074994c5e2518
Asiri Indatissa <asiri@unionassurance.com>
2022-01-26 22:42:20 +0530

    test code for filedataAdd

--- src/App.java
+++ src/App.java
@@ -4,7 +4,6 @@ import service.FileDataAdd;
 import views.CategoryGUI;
 import java.util.ArrayList;
 
-
 public class App {
     public static void main(String[] args) throws Exception {
         // CategoryGUI categoryGUI = new CategoryGUI();
@@ -13,18 +12,19 @@ public class App {
         fileget.fileOperation("transaction.txt");
         System.out.println(fileget.getFileRecordArray());
 
-        
-        ArrayList<ArrayList<String> > aList = 
-        new ArrayList<ArrayList<String> >(1);
-        ArrayList<String> al=new ArrayList<String>();  
-        al.add("Mango");  
-        al.add("Apple"); 
-        aList.add(al);
-        ArrayList<String> al2=new ArrayList<String>();  
-        al2.add("Banana");  
-        al2.add("Grapes"); 
-        aList.add(al2);
+        ArrayList<ArrayList<String>> aList = new ArrayList<ArrayList<String>>(1);
+        ArrayList<String> al = new ArrayList<String>();
+        al.add("Id 01");
+        al.add("Category 01");
+        al.add("1000");
 
+        aList.add(al);
+        ArrayList<String> a2 = new ArrayList<String>();
+
+        a2.add("Id 02");
+        a2.add("Category 02");
+        a2.add("2000");
+        aList.add(a2);
 
         service.FileManager fileadd = new FileDataAdd();
         fileadd.setFileRecordArray(aList);
