dc9cb573d05947c35bf8bf1d473e93d65537a5e8
mkappworks <info.mkappworks@gmail.com>
2022-01-26 08:18:59 +0530

    Added FileManager

---
+++ src/service/FileManager.java
@@ -0,0 +1,18 @@
+package service;
+
+import java.util.ArrayList;
+
+abstract public class FileManager {
+
+    private ArrayList<ArrayList<String>> fileRecordArray;
+
+    public abstract void fileOperation(String filepath);
+
+    public ArrayList<ArrayList<String>> getFileRecordArray() {
+        return fileRecordArray;
+    }
+
+    public void setFileRecordArray(ArrayList<ArrayList<String>> fileRecordArray) {
+        this.fileRecordArray = fileRecordArray;
+    }
+}
