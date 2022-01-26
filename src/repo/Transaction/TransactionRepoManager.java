4a33e4dd726b800c48ab50027a3e035de965f771
mkappworks <info.mkappworks@gmail.com>
2022-01-26 08:29:17 +0530

    Added TransactionRepoManager

---
+++ src/repo/Transaction/TransactionRepoManager.java
@@ -0,0 +1,18 @@
+package repo.Transaction;
+
+import models.Transaction.TransactionList;
+
+public abstract class TransactionRepoManager {
+
+    private TransactionList transactionList;
+
+    public abstract void query();
+
+    public TransactionList getTransactionList() {
+        return transactionList;
+    }
+
+    public void setTransactionList(TransactionList transactionList) {
+        this.transactionList = transactionList;
+    }
+}
