55dfb455441eb8772798bb74049298e41d6f2dfe
mkappworks <info.mkappworks@gmail.com>
2022-01-28 21:40:06 +0530

    Updated Transaction Repo Edit

--- src/repo/Transaction/TransactionRepoEdit.java
+++ src/repo/Transaction/TransactionRepoEdit.java
@@ -1,15 +1,9 @@
 package repo.Transaction;
 
-import models.Transaction.Transaction;
-import models.Transaction.TransactionList;
-
 public class TransactionRepoEdit extends TransactionRepoManager {
-  public void query(Transaction transaction) {
-
-  }
 
   @Override
   public void query() {
-    throw new UnsupportedOperationException();
+
   }
 }
