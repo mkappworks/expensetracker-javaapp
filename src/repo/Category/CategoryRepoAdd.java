517470543a0b49c2cfc55dde6a507c26eff9115f
mkappworks <info.mkappworks@gmail.com>
2022-01-28 21:39:21 +0530

    Updated Category Repo Add

--- src/repo/Category/CategoryRepoAdd.java
+++ src/repo/Category/CategoryRepoAdd.java
@@ -2,34 +2,32 @@ package repo.Category;
 
 import java.util.ArrayList;
 import models.Category.Category;
-import models.Category.CategoryList;
 import service.FileDataAdd;
 import service.FileManager;
 
 public class CategoryRepoAdd extends CategoryRepoManager {
 
-  public void query(Category category) {
+  public void query() {
+    ArrayList<ArrayList<String>> fileRecordArray = new ArrayList<ArrayList<String>>();
+    ArrayList<Category> categoryArray = this.getCategoryList().getList();
 
-    ArrayList<ArrayList<String>> aList = new ArrayList<ArrayList<String>>();
+    for (Category category : categoryArray) {
+      ArrayList<String> stringArray = new ArrayList<String>();
 
-    ArrayList<String> stringArray = new ArrayList<String>();
-    String id = Integer.toString(category.getId());
-    String title = category.getTitle();
-    String budget = Double.toString(category.getBudget());
-    stringArray.add(id);
-    stringArray.add(title);
-    stringArray.add(budget);
+      String id = Integer.toString(category.getId());
+      String title = category.getTitle();
+      String budget = Double.toString(category.getBudget());
 
-    aList.add(stringArray);
+      stringArray.add(id);
+      stringArray.add(title);
+      stringArray.add(budget);
+
+      fileRecordArray.add(stringArray);
+    }
 
     FileManager fileAdd = new FileDataAdd();
-    fileAdd.setFileRecordArray(aList);
+    fileAdd.setFileRecordArray(fileRecordArray);
     fileAdd.fileOperation("category.txt");
   }
 
-  @Override
-  public void query() {
-    throw new UnsupportedOperationException();
-
-  }
 }
