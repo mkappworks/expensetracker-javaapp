9c446719109f63af7e1babcb1f31ad81465ff420
Asiri Indatissa <asiri@unionassurance.com>
2022-01-28 17:38:22 +0530

    first commit

---
+++ src/repo/Transaction/TransactionReoEdit.java
@@ -0,0 +1,15 @@
+package repo.Transaction;
+
+import models.Transaction.Transaction;
+import models.Transaction.TransactionList;
+
+public class TransactionReoEdit extends TransactionRepoManager {
+  public void query(Transaction transaction) {
+
+  }
+
+  @Override
+  public void query() {
+    throw new UnsupportedOperationException();
+  }
+}
