01d57b36f2c57a5bb0033e3370fb0ce9e43b53e3
mkappworks <info.mkappworks@gmail.com>
2022-01-29 11:07:04 +0530

    Updated with TransactionRepoAmend

---
+++ src/repo/Transaction/TransactionRepoAmend.java
@@ -0,0 +1,48 @@
+package repo.Transaction;
+
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+
+import models.Transaction.Transaction;
+import service.FileDataAmend;
+import service.FileManager;
+
+public class TransactionRepoAmend extends TransactionRepoManager {
+
+    @Override
+    public void query() {
+        ArrayList<ArrayList<String>> fileRecordArray = new ArrayList<ArrayList<String>>();
+        ArrayList<Transaction> transactionArray = this.getTransactionList().getList();
+
+        for (Transaction transaction : transactionArray) {
+            ArrayList<String> stringArray = new ArrayList<String>();
+
+            String id = Integer.toString(transaction.getId());
+            String startingDate = new SimpleDateFormat("dd/MM/yyyy").format(transaction.getStartDate());
+            String categoryid = Integer.toString(transaction.getCategory().getId());
+            String categorytitle = transaction.getCategory().getTitle();
+            String categorybudget = Double.toString(transaction.getCategory().getBudget());
+            String note = transaction.getNote();
+            String recurringType = transaction.getRecurringType();
+            String additionalRecurringAmount = Integer.toString(transaction.getAdditionalRecurringAmount());
+            String amount = Double.toString(transaction.getAmount());
+
+            stringArray.add(id);
+            stringArray.add(startingDate);
+            stringArray.add(categoryid);
+            stringArray.add(categorytitle);
+            stringArray.add(categorybudget);
+            stringArray.add(note);
+            stringArray.add(recurringType);
+            stringArray.add(additionalRecurringAmount);
+            stringArray.add(amount);
+
+            fileRecordArray.add(stringArray);
+        }
+
+        FileManager fileAmend = new FileDataAmend();
+        fileAmend.setFileRecordArray(fileRecordArray);
+        fileAmend.fileOperation("transaction.txt");
+    }
+
+}
--- src/repo/Transaction/TransactionRepoEdit.java
+++
@@ -1,9 +0,0 @@
-package repo.Transaction;
-
-public class TransactionRepoEdit extends TransactionRepoManager {
-
-  @Override
-  public void query() {
-
-  }
-}
