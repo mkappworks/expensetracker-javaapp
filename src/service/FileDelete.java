b89d9196d8c3b3eba1bbaa352f5109efc2be462a
Asiri Indatissa <asiri@unionassurance.com>
2022-01-29 08:11:18 +0530

    formatted

--- src/service/FileDelete.java
+++ src/service/FileDelete.java
@@ -1,4 +1,5 @@
 package service;
+
 import java.io.*;
 import java.util.ArrayList;
 
@@ -7,35 +8,32 @@ public class FileDelete extends FileManager {
   @Override
   public void fileOperation(String filepath) {
     ArrayList<ArrayList<String>> fileRecordArray = this.getFileRecordArray();
-    String toDeleteLine ="";
+    String toDeleteLine = "";
     for (ArrayList<String> innerList : fileRecordArray) {
       for (String str : innerList) {
-        //toDeleteLine.concat(str).concat("\t");
+        // toDeleteLine.concat(str).concat("\t");
         toDeleteLine = str + "\t";
       }
     }
 
-    try
-             {
-             File file = new File(filepath);
-             BufferedReader reader = new BufferedReader(new FileReader(file));
-             String line = "", oldtext = "";
-             while((line = reader.readLine()) != null)
-                 {
-                 oldtext += line + "\r\n";
-             }
-             reader.close();
-            
-             //To replace a line in a file
-             String newtext = oldtext.replaceAll(toDeleteLine, "");
-            
-             FileWriter writer = new FileWriter(filepath);
-             writer.write(newtext);writer.close();
-         }
-         catch (IOException ioe)
-             {
-             ioe.printStackTrace();
-         }
+    try {
+      File file = new File(filepath);
+      BufferedReader reader = new BufferedReader(new FileReader(file));
+      String line = "", oldtext = "";
+      while ((line = reader.readLine()) != null) {
+        oldtext += line + "\r\n";
+      }
+      reader.close();
+
+      // To replace a line in a file
+      String newtext = oldtext.replaceAll(toDeleteLine, "");
+
+      FileWriter writer = new FileWriter(filepath);
+      writer.write(newtext);
+      writer.close();
+    } catch (IOException ioe) {
+      ioe.printStackTrace();
+    }
   }
 
 }
