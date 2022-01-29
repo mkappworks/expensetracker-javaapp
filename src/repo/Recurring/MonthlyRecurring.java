c398525e24994e8c57aaa1c3470f9fb16a619296
mkappworks <info.mkappworks@gmail.com>
2022-01-29 11:17:02 +0530

    Added Recurring implementation

---
+++ src/repo/Recurring/MonthlyRecurring.java
@@ -0,0 +1,14 @@
+package repo.Recurring;
+
+import models.Transaction.TransactionEntryList;
+import models.Transaction.TransactionList;
+
+public class MonthlyRecurring implements TransactionRecurringManager {
+
+    @Override
+    public TransactionEntryList getRecurringList(TransactionList transactionList) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+    
+}
---
+++ src/repo/Recurring/NoRecurring.java
@@ -0,0 +1,14 @@
+package repo.Recurring;
+
+import models.Transaction.TransactionEntryList;
+import models.Transaction.TransactionList;
+
+public class NoRecurring implements TransactionRecurringManager {
+
+    @Override
+    public TransactionEntryList getRecurringList(TransactionList transactionList) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+    
+}
