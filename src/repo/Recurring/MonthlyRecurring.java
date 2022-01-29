e235a1d319d444d863626c33cb81ed2a8a87e99d
mkappworks <info.mkappworks@gmail.com>
2022-01-29 11:57:41 +0530

    Added MonthlyRecurring

--- src/repo/Recurring/MonthlyRecurring.java
+++ src/repo/Recurring/MonthlyRecurring.java
@@ -1,5 +1,13 @@
 package repo.Recurring;
 
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Date;
+
+import models.Category.Category;
+import models.Transaction.Transaction;
+import models.Transaction.TransactionData;
+import models.Transaction.TransactionEntry;
 import models.Transaction.TransactionEntryList;
 import models.Transaction.TransactionList;
 
@@ -7,8 +15,32 @@ public class MonthlyRecurring implements TransactionRecurringManager {
 
     @Override
     public TransactionEntryList getRecurringList(TransactionList transactionList) {
-        // TODO Auto-generated method stub
-        return null;
+        ArrayList<Transaction> transactionArrayList = transactionList.getList();
+        ArrayList<TransactionEntry> transactionEntryArrayList = new ArrayList<TransactionEntry>();
+
+        for (Transaction transaction : transactionArrayList) {
+            Date startingDate = transaction.getStartDate();
+            Category category = transaction.getCategory();
+            String note = transaction.getNote();
+            String transactionType = transaction.getTransactionType();
+            int additionalRecurringAmount = transaction.getAdditionalRecurringAmount();
+            double amount = transaction.getAmount();
+            TransactionData transactionData = new TransactionData(category, transactionType, note, amount);
+
+            for (int i = 0; i <= additionalRecurringAmount; i++) {
+                Date date = startingDate;
+                Calendar calender = Calendar.getInstance();
+                calender.setTime(startingDate);
+                calender.add(Calendar.MONTH, i);
+                date = calender.getTime();
+
+                TransactionEntry transactionEntry = new TransactionEntry(date, transactionData);
+                transactionEntryArrayList.add(transactionEntry);
+            }
+
+        }
+
+        return new TransactionEntryList(transactionEntryArrayList);
     }
-    
+
 }
