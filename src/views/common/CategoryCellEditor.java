14cb0482814ae22c6aca4aa77c73dc7ed01bde96
mkappworks <info.mkappworks@gmail.com>
2022-01-30 23:06:26 +0530

    Added CategoryCellEditor

---
+++ src/views/common/CategoryCellEditor.java
@@ -0,0 +1,72 @@
+package views.common;
+
+import java.awt.Component;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.List;
+
+import javax.swing.AbstractCellEditor;
+import javax.swing.JComboBox;
+import javax.swing.JTable;
+import javax.swing.table.TableCellEditor;
+
+import models.Category.Category;
+
+public class CategoryCellEditor extends AbstractCellEditor
+        implements TableCellEditor, ActionListener {
+
+    private Category category;
+    private List<Category> listCategory;
+
+    public CategoryCellEditor(List<Category> listCategory) {
+        this.listCategory = listCategory;
+    }
+
+    @Override
+    public Object getCellEditorValue() {
+        return this.category;
+    }
+
+    @Override
+    public Component getTableCellEditorComponent(JTable table, Object value,
+            boolean isSelected, int row, int column) {
+        if (value instanceof Category) {
+            this.category = (Category) value;
+        }
+
+        JComboBox<Category> comboCategory = new JComboBox<Category>();
+
+        for (Category categoryRecord : listCategory) {
+            comboCategory.addItem(categoryRecord);
+        }
+
+        comboCategory.setSelectedItem(category);
+        comboCategory.addActionListener(this);
+
+        if (isSelected) {
+            comboCategory.setBackground(table.getSelectionBackground());
+        } else {
+            comboCategory.setBackground(table.getSelectionForeground());
+        }
+
+        return comboCategory;
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent event) {
+        JComboBox<Category> comboCategory = (JComboBox<Category>) event.getSource();
+        this.category = (Category) comboCategory.getSelectedItem();
+    }
+
+    // @Override
+    // public boolean equals(Object obj) {
+    // if (obj == null)
+    // return false;
+    // if (obj.getClass() != Category.class)
+    // return false;
+    // if (getName().equals(((Category) obj).getTitle()))
+    // return true;
+    // return false;
+    // }
+
+}
